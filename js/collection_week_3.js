553177736d8087ab278010de25fb20f1dfcc41d9
michellemseo <112968364+michellemseo@users.noreply.github.com>
2022-11-15 21:19:12 -0500

    update

--- js/collection_week_3.js
+++ js/collection_week_3.js
@@ -1,4 +1,4 @@
-var colors = ["red", "pink", "blue", "yellow", "brown", "green"];
+var colors = ["darkred", "pink", "orange", "skyblue", "lightgreen", "teal"];
 
 for(let i = 0; i<6; i++){
    var chosenColorNumber = Math.floor(Math.random()*6);
@@ -10,14 +10,14 @@ for(let i = 0; i<6; i++){
     
     
     $("#square" + i).css({"position":"absolute", "top":positiontop + "%"})
-     $("#square" + i).css({"top":positiontop + "%"})
+    $("#square" + i).css({"top":positiontop + "%"})
     positionleft = "calc("+ positionleft + "% - 100px)";
     console.log(positionleft);
     $("#square" + i).css({"left":positionleft});
     $("#square" + i).css({"background-color":chosenColor});
-    $("#square" + i).css({"z-index":-3*i-1});
+    $("#square" + i).css({"z-index":-1*i-2});
     
-    var sizewidth = Math.floor(Math.random()*100);
+    var sizewidth = Math.floor(Math.random()*150);
     var sizeheight = Math.floor(Math.random()*200);
     console.log(sizewidth);
     $("#square" + i).css({"width":sizewidth + "px"});
